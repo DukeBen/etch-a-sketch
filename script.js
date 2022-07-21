f0c4e63c653026bd612fd71caab9e0289cd7bb2c
Ben Goldfried <2goldenfried@gmail.com>
2022-07-21 00:23:22 -0400

    Add range slider instead of prompt

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const container = document.querySelector('div.container');
-const size = prompt("What size do you want")
-for(let i = 0; i < size * size; i++)
-{
+const barVal = document.querySelector('#bar');
+barVal.addEventListener('mouseup', function(e){
+    deleteGrid();
+    createGrid(e.target.value);
+});
+
+createGrid(16) //default grid
+function createGrid(size){
+    for(let i = 0; i < size * size; i++)
+    {
     let gridElement = document.createElement('div');
     gridElement.classList.add('gridElement')
         /*Pass JS values into CSS ... Super useful!! 
@@ -10,14 +17,27 @@ for(let i = 0; i < size * size; i++)
     gridElement.style.setProperty('--grid-height', 560/size);
     gridElement.style.setProperty('--grid-width', 560/size);
     container.appendChild(gridElement);
+    }
+    divs = document.querySelectorAll('div.gridElement');
+        divs.forEach(div => {
+         div.addEventListener('mouseover', function(e){
+             e.target.classList.add('hovered');
+            })
+        })
 }
 
-const divs = document.querySelectorAll('div.gridElement');
-divs.forEach(div => {
-    div.addEventListener('mouseover', function(e){
-        e.target.classList.add('hovered');
-    })
-})
+
 
 const clear = document.querySelector('#clear');
-clear.addEventListener('click', () => divs.forEach(div => div.classList.remove('hovered')));
+clear.addEventListener('click', clearGrid);
+function clearGrid ()
+{
+    divs = document.querySelectorAll('div.gridElement');
+    divs.forEach(div => div.classList.remove('hovered'));
+}
+
+function deleteGrid()
+{
+    divs = document.querySelectorAll('div.gridElement');
+    divs.forEach(div => div.remove());
+}
